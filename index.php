11145d5bff14c7ce483bc4d71e1e026e3ca3232d
tanawat <t1209000014090@hotmail.com>
2021-01-15 14:39:59 +0700

    edit index

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 echo "My Homepage";
 ?>
-<a href="page1.php"></a>
-<a href="page2.php"></a>
-<a href="page3.php"></a>
-<a href="page4.php"></a>
+<a href="page1.php">page1</a>
+<a href="page2.php">page2</a>
+<a href="page3.php">page3</a>
+<a href="page4.php">page4</a> 
+<a href="page5.php">page5</a> 
